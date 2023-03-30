373204f6302ede4bf12120ed17baaff6dd86bba7
m.shymechko <m.shymechko@brainence.com>
2023-03-30 12:35:57 +0300

    first commit

---
+++ ChatService/Program.cs
@@ -0,0 +1,29 @@
+﻿using ChatService.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddSignalR();
+
+builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+{
+    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
+
+builder.Services.AddSingleton<IDictionary<string, ChatService.UserConnection>>( opts => new Dictionary<string, ChatService.UserConnection>());
+
+var app = builder.Build();
+
+app.UseCors(builder => builder
+.AllowAnyHeader()
+.AllowAnyMethod()
+.SetIsOriginAllowed((host) => true)
+.AllowCredentials());
+
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapHub<ChatHub>("/chat");
+
+app.Run();
+
---
+++ ChatService/UserConnection.cs
@@ -0,0 +1,10 @@
+﻿namespace ChatService
+{
+	public class UserConnection
+	{
+		public string User { get; set; }
+
+		public string Room { get; set; }
+	}
+}
+
